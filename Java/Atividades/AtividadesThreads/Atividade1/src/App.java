2e0ed77d38a208765fbac1bc375f6d71f116cf2b
HeitorDalla <heitorvillavilla@gmail.com>
2025-11-25 20:46:51 -0300

    adicionando prioridade nas threads

--- Java/Atividades/AtividadesThreads/Atividade1/src/App.java
+++ Java/Atividades/AtividadesThreads/Atividade1/src/App.java
@@ -1,7 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // TRABALHAR COM PRIORIDADES (VAI DE 1 À 10) - definir prioridades diferentes paras as threads
         A obj1 = new A();
         B obj2 = new B();
+
+        obj2.setPriority(1);
 
         obj1.start();
         obj2.start();
@@ -12,6 +15,11 @@ class A extends Thread { // no momento em que extendo de Thread, a classe não �
     public void run () {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hi");
+            try {
+                Thread.sleep(1); // definir o tempo que voce quer que essa thread fique em pausa
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -20,6 +28,11 @@ class B extends Thread {
     public void run () {
         for (int i = 0; i < 100; i++) {
             System.out.println("Ola");
+            try {
+                Thread.sleep(1); // definir o tempo que voce quer que essa thread fique em pausa
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
