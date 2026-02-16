c120d48d81dfc25b278e5f3169101915f94254e2
HeitorDalla <heitorvillavilla@gmail.com>
2026-02-16 10:43:42 -0300

    feat: adicionando arquivo Dockerfile.dev para testes localmente para hot reloading

--- Docker/react-app/src/App.tsx
+++ Docker/react-app/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Teste que editeiiiiiiiiiiii
+          Test
         </p>
         <a
           className="App-link"
@@ -18,7 +18,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> 
     </div>
   );
 }
